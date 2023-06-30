6a527f9ec119b08328cccfd28ed654d2492b7b82
Richard Madison <rmadison@lightspeedtech.com>
2023-06-30 10:43:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
-    echo "hi";
+
+$localfilename = "builder.zip";
+$localzip = fopen($localfilename, "w+");
+
+if (flock($localzip, LOCK_EX)) {
+    fwrite($localzip, fopen("https://control-nyc001.sitebuilderapp.com/builder/builder.zip", 'r'));
+    $zip = new ZipArchive;
+    $res = $zip->open($localfilename);
+    if ($res === TRUE) {
+      $zip->extractTo('.');
+      $zip->close();
+      //
+    } else {
+      //
+    }
+    flock($file, LOCK_UN);
+} else {
+    die("Couldn't download the zip file.");
+}
+
+fclose($localzip);
