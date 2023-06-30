8678346d82e4d3e989b7107befc47cd0f121a33e
Richard Madison <rmadison@lightspeedtech.com>
2023-06-30 19:40:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,19 @@ file_put_contents($localfile, fopen($remotefile, 'r'));
 
 unlink(__FILE__);
 
+$path = getcwd();
+$all = explode("/",$path);
+$size = sizeof($all);
+$folder = "../" . $all[$size - 1];
+
 $zip = new ZipArchive;
 $res = $zip->open($localfile);
 if ($res === TRUE) {
-  $zip->extractTo('.');
+  $zip->extractTo($folder);
   $zip->close();
   //
 } else {
   //
 }
+
+unlink($localfile);
