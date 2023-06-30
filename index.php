0034a1d256c5125ef696e9cb325cbb4c5e2732ee
Richard Madison <rmadison@lightspeedtech.com>
2023-06-30 12:12:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php
+    echo "<pre>";
+    print_r($_SERVER);
+    echo "</pre>";
+    die();
 
-$localfilename = "builder.zip";
-$localzip = fopen($localfilename, "w+");
+// $localfilename = "builder.zip";
+// $localzip = fopen($localfilename, "w+");
 
-if (flock($localzip, LOCK_EX)) {
-    fwrite($localzip, fopen("https://control-nyc001.sitebuilderapp.com/builder/builder.zip", 'r'));
-    $zip = new ZipArchive;
-    $res = $zip->open($localfilename);
-    if ($res === TRUE) {
-      $zip->extractTo('.');
-      $zip->close();
-      //
-    } else {
-      //
-    }
-    flock($file, LOCK_UN);
-} else {
-    die("Couldn't download the zip file.");
-}
+// if (flock($localzip, LOCK_EX)) {
+//     fwrite($localzip, fopen("https://control-nyc001.sitebuilderapp.com/builder/builder.zip", 'r'));
+//     $zip = new ZipArchive;
+//     $res = $zip->open($localfilename);
+//     if ($res === TRUE) {
+//       $zip->extractTo('.');
+//       $zip->close();
+//       //
+//     } else {
+//       //
+//     }
+//     flock($file, LOCK_UN);
+// } else {
+//     die("Couldn't download the zip file.");
+// }
 
-fclose($localzip);
+// fclose($localzip);
